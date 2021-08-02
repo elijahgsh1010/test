e68f4c7fc9634974a9320acf13ed288ba80b821b
elijahgsh1010 <elijah@surer.sg>
2021-08-02 23:17:56 +0800

    cleanup

--- aspnet-core/src/Test.Application.Contracts/SurerClient/GetFamilyForView.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Test.SurerClient
-{
-    public class GetFamilyForView
-    {
-        public long Id { get; set; }
-        public string NucleusName { get; set; }
-        public string MailingAddress { get; set; }
-    }
-}

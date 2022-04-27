2c81fef136f32b20e3ce0e988e668b6a60fc055f
elijah <elijahgsh@gmail.com>
2022-04-27 16:05:25 +0800

    commit #1

--- aspnet-core/src/Test.Application/TestAppService.cs
+++ aspnet-core/src/Test.Application/TestAppService.cs
@@ -3,6 +3,7 @@ using Volo.Abp.Application.Services;
 
 namespace Test
 {
+    // test commit 1
     /* Inherit your application services from this class.
      */
     public abstract class TestAppService : ApplicationService
